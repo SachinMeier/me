82bb44b8a0866586dad8ad08db7f372809032e8c
Pillagr <Pillagr6@gmail.com>
2020-10-16 22:15:59 -0700

    cleanup

--- php/mail.php
+++
@@ -1,8 +0,0 @@
-<?php $name = $_POST['name'];
-$formcontent="From: $name \n Message: $addr1 \n $addr2";
-$recipient = "sachin.meier@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $name \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
-?>
